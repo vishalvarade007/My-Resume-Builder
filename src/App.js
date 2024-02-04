04d35777593fbd0174f009d0b1f1d02fe2cb49ed
varadevishal07@gmail.com <varadevishal07@gmail.com>
2024-02-04 11:27:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Routes, Route ,Navigate} from 'react-router-dom';
+import DetailsFillingPage from './Components/DetailsFillComponents/DetailsFillingPage';
+import Home from './Components/HomePage/Home'
+import NavBar from './Components/Navigation/Navbar'
+import MyResume from './Components/ResumeDisplay/MyResume';
+import AboutUs from './Components/AboutUs/AboutUs';
 import './App.css';
 
+// This component defines routing for other components 
+// Home page displays the different templates from which user can select either of them.
+//'detailsfillingpage' displays the detailsfillingpage inside which there are nested routes to display personalinfo, workexperience, education and keyskills page.
+//'myresume' displays the resume created by the user. 
+//'about' displays the AboutUs page of the app.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div> 
+          <NavBar/>
+        </div>
+        
+        <div>
+            <Routes>
+                  <Route exact path="/" element={<Home/>}></Route>
+                  <Route path="/detailsfillingpage/*" element ={<DetailsFillingPage />}></Route> 
+                  <Route path="/myresume" element={<MyResume/>}></Route>
+                  <Route exact path="/about" element={<AboutUs/>}></Route>
+                  <Route path="*" element={<Navigate to="/about" />}></Route>
+            </Routes> 
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
+
+
